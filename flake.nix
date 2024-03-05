dc4513fe4309c8aa96a19f0f990c923d1c0ff4c6
Bryan Jones <unix.chkpwd@chkpwd.com>
2024-03-05 06:27:26 +0000

    testing

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
     };
     vscode-server.url = "github:nix-community/nixos-vscode-server";
   };
+
   outputs = {
     self,
     nixpkgs,
@@ -15,9 +16,9 @@
     home-manager,
     vscode-server,
     ...
-  }: let
-    username = "chk";
-    sshPubKey = "ssh-ed25519 CHKTHISKEY";
+  }: inputs let
+    username = "chkpwd";
+    sshPubKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBK2VnKgOX7i1ISETheqjAO3/xo6D9n7QbWyfDAPsXwa";
   in {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
@@ -30,9 +31,36 @@
 
           services.vscode-server.enable = true;
 
+          environment = {
+            systemPackages = with pkgs; [
+              vim
+              htop
+              wget
+            ];
+            pathsToLink = ["/share/zsh"];
+            shells = [pkgs.zsh];
+          };
+
+          nix = {
+            gc = {
+              automatic = true;
+              dates = "weekly";
+              options = "--delete-older-than 7d";
+            };
+            settings = {
+              experimental-features = ["nix-command" "flakes"];
+              auto-optimise-store = true;
+              accept-flake-config = true;
+              trusted-users = ["root" "@wheel"];
+              substituters = [
+                "https://nix-community.cachix.org"
+                "https://cache.nixos.org/"
+              ];
+            };
+          };
+
           wsl = {
             enable = true;
-            wslConf.automount.root = "/mnt";
             defaultUser = username;
             startMenuLaunchers = true;
             nativeSystemd = true;
@@ -44,11 +72,13 @@
             users.${username} = {
               isNormalUser = true;
               extraGroups = ["wheel"];
-              shell = pkgs.fish;
               openssh.authorizedKeys.keys = [sshPubKey];
             };
           };
-
+          
+          # Allow Proprietary software
+          nixpkgs.config.allowUnfree = true;
+          
           home-manager = {
             useGlobalPkgs = true;
             useUserPackages = true;
@@ -59,9 +89,18 @@
                   homeDirectory = "/home/${username}";
                   stateVersion = "23.11";
                   packages = with pkgs; [curl wget];
+                  file = {
+                    vscode = {
+                      target = ".vscode-server/.env";
+                      text = ''
+                        # Add system pkgs
+                        PATH=$PATH:/run/current-system/sw/bin/
+                      '';
+                    };
+                  };
                 };
-
                 programs.home-manager.enable = true;
+                programs.zsh.enable = true;
                 programs.git.enable = true;
                 services.ssh-agent.enable = true;
               }
