a8b1640e86d677ff24f471a24b1e44e0ae064b15
Bryan Jones <unix.chkpwd@chkpwd.com>
2024-03-07 16:43:39 +0000

    feat: add chezmoi script

--- hosts/wsl.nix
+++ hosts/wsl.nix
@@ -2,7 +2,6 @@
   pkgs,
   inputs,
   config,
-  lib,
   username,
   ...
 }: {
@@ -44,7 +43,7 @@
   home-manager = {
     users.${username}.imports = [
       ../modules/home/dev
-      {
+      ({ config, lib, ...}: {
         home.file = {
           ".vscode-server/.env" = {
           text = ''
@@ -54,23 +53,28 @@
           };
         };
 
-        home.activation = {
-          pre-chezmoi = ''
-            # Create Chezmoi configuration file with access token
-            $DRY_RUN_CMD mkdir -p $HOME/.config/chezmoi
-            $DRY_RUN_CMD \
-            echo "data:
-              accessToken: $(cat ${config.sops.secrets.chezmoi_token.path})" > "$HOME/.config/chezmoi/chezmoi.yml"
-          '';
-          # post-chezmoi = lib.hm.dag.entryAfter ["writeBoundary"] ''
-          post-chezmoi = ''
-            $DRY_RUN_CMD ${pkgs.chezmoi}/bin/chezmoi init --apply ${username}
-          '';
-        };
-
         programs.zsh.enable = true;
         programs.git.enable = true;
-      }
+      })
     ];
+  };
+
+  systemd.services."chezmoi-init" = {
+    description = "Initialize Chezmoi";
+    after = ["network.target"];
+    wantedBy = ["multi-user.target"];
+    script = ''
+      mkdir -p $HOME/.config/chezmoi
+      echo "
+      data:
+        accessToken: $(cat ${config.sops.secrets.chezmoi_token.path})
+      " > $HOME/.config/chezmoi/chezmoi.yml
+      "${pkgs.chezmoi}/bin/chezmoi" -- init --apply ${username}
+    '';
+    serviceConfig = {
+      User = username;
+      WorkingDirectory = "/home/${username}";
+      Type = "oneshot";
+    };
   };
 }
