5d8229420d0e9b3d55cef9179ad7c5aa218afe21
Bryan Jones <unix.chkpwd@gmail.com>
2024-06-25 16:20:44 -0400

    adding new configs

--- hosts/nix-mb-01.nix
+++ hosts/nix-mb-01.nix
@@ -1,7 +1,29 @@
-{inputs, ...}: {
-  # Enable Nix Daemon
-  nix.useDaemon = true;
+{
+  inputs,
+  pkgs,
+  sshPubKey,
+  ...
+}: {
+  config = {
+    networking.hostName = nix-mb-01;
 
-  # Sudo Touch ID authentication
-  security.pam.enableSudoTouchIdAuth = true;
+    users.users.scotte = {
+      name = "chkpwd";
+      home = "/Users/chkpwd";
+      shell = pkgs.zsh;
+      packages = [pkgs.home-manager];
+      openssh.authorizedKeys.keys = sshPubKey;
+    };
+
+    homebrew = {
+      taps = [
+      ];
+      brews = [
+      ];
+      casks = [
+        "alacritty"
+      ];
+      masApps = {};
+    };
+  };
 }
