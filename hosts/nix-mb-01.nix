c506fa1a9c01d3c4e47a20e6c8258440cf5c5fb2
Bryan Jones <unix.chkpwd@gmail.com>
2024-06-25 20:41:08 -0400

    testing simpler commit

--- hosts/nix-mb-01.nix
+++ hosts/nix-mb-01.nix
@@ -1,29 +1,4 @@
+{ pkgs, ... }:
 {
-  inputs,
-  pkgs,
-  sshPubKey,
-  ...
-}: {
-  config = {
-    networking.hostName = "nix-mb-01";
-
-    users.users.chkpwd = {
-      name = "chkpwd";
-      home = "/Users/chkpwd";
-      shell = pkgs.zsh;
-      packages = [pkgs.home-manager];
-      openssh.authorizedKeys.keys = sshPubKey;
-    };
-
-    homebrew = {
-      taps = [
-      ];
-      brews = [
-      ];
-      casks = [
-        "alacritty"
-      ];
-      masApps = {};
-    };
-  };
+ services.nix-daemon.enable = true;
 }
