6972fa8d3a910c4c645352daf0487ce1f6479f50
Bryan Jones <unix.chkpwd@gmail.com>
2024-08-22 08:35:57 -0400

    changed preseed

--- modules/nixos/incus.nix
+++ modules/nixos/incus.nix
@@ -12,52 +12,66 @@ in {
   };
 
   config = mkIf cfg.enable {
-    virtualisation = {
-      incus = {
-        enable = true;
-        ui.enable = true;
-        preseed = {
-          config = {
-            core.https_address = "172.16.16.150:9999";
-            images.auto_update_interval = 6;
-          };
-          networks = [
-            {
-              config = {
-                "ipv4.address" = "172.16.16.150/24";
-                "ipv4.nat" = "true";
-              };
-              name = "incusbr0";
-              type = "bridge";
-            }
-          ];
-          profiles = [
-            {
-              devices = {
-                eth0 = {
-                  name = "eth0";
-                  network = "incusbr0";
-                  type = "nic";
-                };
-                root = {
-                  path = "/";
-                  pool = "default";
-                  type = "disk";
-                };
-              };
-              name = "default";
-            }
-          ];
-          storage_pools = [
-            {
-              config = {
-                source = "/var/lib/incus/storage-pools/default";
-              };
-              driver = "dir";
-              name = "default";
-            }
-          ];
+    virtualisation.incus = {
+      enable = true;
+      ui.enable = true;
+      preseed = {
+        config = {
+          "core.https_address" = "[::]:8443";
         };
+        networks = [
+          {
+            name = "incusbr0";
+            type = "bridge";
+            config = {
+              "ipv4.address" = "172.16.16.150/24";
+              "ipv4.nat" = true;
+              "ipv6.address" = "auto";
+            };
+          }
+        ];
+        # storage_pools = [
+        #   {
+        #     config.source = "/vm-data";
+        #     driver = "dir";
+        #     name = "default";
+        #   }
+        # ];
+        profiles = [
+          {
+            name = "default";
+            devices = {
+              eth0 = {
+                name = "eth0";
+                type = "nic";
+                network = "incusbr0";
+              };
+              root = {
+                path = "/";
+                pool = "default";
+                size = "20GB";
+                type = "disk";
+              };
+            };
+          }
+          {
+            name = "external";
+            devices = {
+              eth1 = {
+                name = "eth1";
+                type = "nic";
+                nictype = "bridged";
+                parent = "br0";
+              };
+              root = {
+                path = "/";
+                pool = "default";
+                size = "20GB";
+                type = "disk";
+              };
+            };
+          }
+        ];
       };
     };
     users.users.${username}.extraGroups = ["incus-admin"];
