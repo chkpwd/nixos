15a6fa860fa62e1b96a96ce047a9f6cd7d300262
Bryan Jones <unix.chkpwd@gmail.com>
2024-09-09 09:51:25 -0400

    removed some features due to macos issues

--- modules/common/settings.nix
+++ modules/common/settings.nix
@@ -14,7 +14,7 @@ in {
     optimise.automatic = false; # https://github.com/LnL7/nix-darwin/pull/915#issuecomment-2165177905
     settings = {
       experimental-features = ["nix-command" "flakes"];
-      auto-optimise-store = true;
+      auto-optimise-store = false; # https://github.com/NixOS/nix/issues/7273
       accept-flake-config = true;
       trusted-users = mkDefault ["root" "@wheel"];
       substituters = [
