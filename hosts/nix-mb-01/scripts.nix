4ca861f89c7e49d5ac8dc67b8e3b34e001dd0eba
Bryan Jones <unix.chkpwd@gmail.com>
2024-10-26 11:17:11 -0400

    added new macos defaults

--- hosts/nix-mb-01/scripts.nix
+++ hosts/nix-mb-01/scripts.nix
@@ -3,6 +3,7 @@ _: {
   system.activationScripts.postUserActivation.text = ''
     echo "Setting UI Defaults..."
 
+    #- File System -#
     # Create default directory for Screenshots
     mkdir -p /Users/chkpwd/Documents/Screenshots
 
@@ -17,5 +18,30 @@ _: {
     # Avoid creating .DS_Store files on network or USB volumes
     defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
     defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+    #- Gestures -#
+    defaults write com.apple.trackpad.threeFingerVertSwipeGesture -int "0"
+    defaults write com.apple.WindowManager.EnableTiledWindowMargins -int "0"
+
+    #- Menu Bar -#
+    # Hide items from menubar
+    for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
+        defaults write "$\{domain}" dontAutoLoad -array \
+          "/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
+          "/System/Library/CoreServices/Menu Extras/Volume.menu" \
+          "/System/Library/CoreServices/Menu Extras/User.menu"
+    done
+
+    # Show Bluetooth, AirPort, Battery, and Clock
+    defaults write com.apple.systemuiserver menuExtras -array \
+      "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
+      "/System/Library/CoreServices/Menu Extras/AirPort.menu"
+
+    defaults write com.apple.controlcenter Battery -int "1"
+    defaults write com.apple.controlcenter Hearing -int "2"
+
+    #- Window Management -#
+    # Disable margins around tiled windows
+    defaults write com.apple.WindowManager EnableTiledWindowMargins -int "0"
   '';
 }
