70de25d400f6cc87224a1342926b1ab07e229fb5
Bryan Jones <unix.chkpwd@gmail.com>
2024-11-01 13:44:27 -0400

    set default

--- deploy.nix
+++ deploy.nix
@@ -4,7 +4,7 @@ let
     hostname = name;
     profiles.system = {
       user = "root";
-      sshUser = ""; # lib.strings.splitString "\n" (builtins.readFile ../../homes/bjw-s/config/ssh/ssh.pub);
+      sshUser = "chkpwd";
       path = inputs.deploy-rs.lib.${system}.activate.nixos self.nixosConfigurations.${name};
       magicRollback = true;
       sshOpts = [ ];
@@ -17,7 +17,6 @@ in
     nix-vm-01 = deployConfig "nix-vm-01" "x86_64-linux" { };
     nix-wsl-01 = deployConfig "nix-wsl-01" "x86_64-linux" { };
     nix-host-01 = deployConfig "nix-host-01" "x86_64-linux" {
-      sshUser = "chkpwd";
       remoteBuild = true;
     };
   };
