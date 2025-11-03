5d9260dd6dd339454d656fdccc47579fbae047d1
Bryan Jones <unix.chkpwd@gmail.com>
2025-11-03 15:50:10 -0500

    removed unused k8s pkgs

--- modules/common/packages/k8s.nix
+++ modules/common/packages/k8s.nix
@@ -16,17 +16,13 @@ in
   config = mkIf cfg.enableKubernetesTools {
     users.users.${config.crossSystem.username}.packages = with pkgs; [
       hubble
-      kubecolor
       viddy
-      nova
-      pluto
       kubectl
       kubernetes-helm
       kustomize
       cmctl
       unstable.fluxcd
       cilium-cli
-      k9s
       kind
     ];
   };
